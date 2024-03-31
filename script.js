8f2a73b8f757978c64ebcc8a0e033fcdb95be1e9
sarcaxticlarka <140724625+sarcaxticlarka@users.noreply.github.com>
2024-03-31 18:46:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,375 @@
+let user = [
+     
+    {
+        profilePic: "https://images.unsplash.com/photo-1503104834685-7205e8607eb9?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        displayPic: "https://images.unsplash.com/photo-1628657487151-055f43d70667?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        pendingMessage: 5,
+        location: "Lucknow, India",
+        name: "Neha",
+        age: 23,
+        interest: [{
+            icon: `<i class="ri-music-2-fill"></i>`,
+            interest: "music",
+        },
+        {
+            icon: `<i class="ri-quill-pen-fill"></i>`,
+            interest: "writing",
+        },
+        {
+            icon: `<i class="ri-paint-fill"></i>`,
+            interest: "painting",
+        }
+        ],
+        bio: "The only limit to our realization of tomorrow will be our doubts of today.",
+        isFriends: null
+    },
+    // {
+    //     profilePic: "https://images.unsplash.com/photo-1480406537807-00c0914e8d40?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    //     displayPic: "https://images.unsplash.com/photo-1565720517886-fb952968e0f2?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    //     pendingMessage: 5,
+    //     location: "USA, California",
+    //     name: "Mira",
+    //     age: 29,
+    //     interest: [{
+    //         icon: `<i class="ri-music-2-fill"></i>`,
+    //         interest: "music",
+    //     },
+    //     {
+    //         icon: `<i class="ri-quill-pen-fill"></i>`,
+    //         interest: "writing",
+    //     },
+    //     {
+    //         icon: `<i class="ri-paint-fill"></i>`,
+    //         interest: "painting",
+    //     }
+    //     ],
+    //     bio: "What you get by achieving your goals is not as important as what you become by achieving your goals.",
+    //     isFriends: null
+    // },
+    {
+        profilePic: "https://images.unsplash.com/photo-1508341591423-4347099e1f19?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        displayPic: "https://images.unsplash.com/photo-1546572797-e8c933a75a1f?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        pendingMessage: 4,
+        location: "Mumbai, India",
+        name: "Rahul",
+        age: 22,
+        interest: [{
+            icon: `<i class="ri-music-2-fill"></i>`,
+            interest: "music",
+        },
+        {
+            icon: `<i class="ri-quill-pen-fill"></i>`,
+            interest: "writing",
+        },
+        {
+            icon: `<i class="ri-paint-fill"></i>`,
+            interest: "painting",
+        }
+        ],
+        bio: "The best way to predict the future is to create it.",
+        isFriends: null
+    },
+    {
+        profilePic: "https://plus.unsplash.com/premium_photo-1664015982598-283bcdc9cae8?q=80&w=1665&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        displayPic: "https://images.unsplash.com/photo-1654850900859-be34bd2e217c?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        pendingMessage: 3,
+        location: "Bangalore, India",
+        name: "Aarav",
+        age: 27,
+        interest: [{
+            icon: `<i class="ri-music-2-fill"></i>`,
+            interest: "music",
+        },
+        {
+            icon: `<i class="ri-quill-pen-fill"></i>`,
+            interest: "writing",
+        },
+        {
+            icon: `<i class="ri-paint-fill"></i>`,
+            interest: "painting",
+        }
+        ],
+        bio: "The only way to do great work is to love what you do.",
+        isFriends: null
+    },
+    {
+        profilePic: "https://images.unsplash.com/photo-1439778615639-28529f7628bc?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        pendingMessage: 2,
+        location: "Chennai, India",
+        name: "Kavya",
+        age: 25,
+        interest: [{
+            icon: `<i class="ri-music-2-fill"></i>`,
+            interest: "music",
+        },
+        {
+            icon: `<i class="ri-quill-pen-fill"></i>`,
+            interest: "writing",
+        },
+        {
+            icon: `<i class="ri-paint-fill"></i>`,
+            interest: "painting",
+        }
+        ],
+        bio: "Believe you can and you're halfway there.",
+        isFriends: null
+    },
+    {
+        profilePic: "https://images.unsplash.com/photo-1601290346815-8b4eb9cf6b1e?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        displayPic: "https://images.unsplash.com/photo-1618747946260-9511b46b1ac7?q=80&w=1618&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        pendingMessage: 1,
+        location: "Hyderabad, India",
+        name: "Aditi",
+        age: 30,
+        interest: [{
+            icon: `<i class="ri-music-2-fill"></i>`,
+            interest: "music",
+        },
+        {
+            icon: `<i class="ri-quill-pen-fill"></i>`,
+            interest: "writing",
+        },
+        {
+            icon: `<i class="ri-paint-fill"></i>`,
+            interest: "painting",
+        }
+        ],
+        bio: "The future belongs to those who believe in the beauty of their dreams.",
+        isFriends: null
+    },
+    {
+        profilePic: "https://images.unsplash.com/photo-1558700009-7ffb7a8e8e88?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        displayPic: "https://images.unsplash.com/photo-1581841064838-a470c740e8ee?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        pendingMessage: 0,
+        location: "Pune, India",
+        name: "Riya",
+        age: 28,
+        interest: [{
+            icon: `<i class="ri-music-2-fill"></i>`,
+            interest: "music",
+        },
+        {
+            icon: `<i class="ri-quill-pen-fill"></i>`,
+            interest: "writing",
+        },
+        {
+            icon: `<i class="ri-paint-fill"></i>`,
+            interest: "painting",
+        }
+        ],
+        bio: "The future belongs to those who believe in the beauty of their dreams.",
+        isFriends: null
+    },
+    {
+        profilePic: "https://images.unsplash.com/photo-1598189247207-233a2067b999?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        displayPic: "https://images.unsplash.com/photo-1524504388940-b1c1722653e1?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        pendingMessage: 0,
+        location: "Kolkata, India",
+        name: "Ananya",
+        age: 24,
+        interest: [{
+            icon: `<i class="ri-music-2-fill"></i>`,
+            interest: "music",
+        },
+        {
+            icon: `<i class="ri-quill-pen-fill"></i>`,
+            interest: "writing",
+        },
+        {
+            icon: `<i class="ri-paint-fill"></i>`,
+            interest: "painting",
+        }
+        ],
+        bio: "The only way to do great work is to love what you do.",
+        isFriends: null
+    },
+    {
+        profilePic: "https://images.unsplash.com/photo-1544179559-032b931c661e?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        displayPic: "https://images.unsplash.com/photo-1519085360753-af0119f7cbe7?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        pendingMessage: 4,
+        location: "Delhi, India",
+        name: "Ashay",
+        age: 24,
+        interest: [{
+            icon: `<i class="ri-music-2-fill"></i>`,
+            interest: "music",
+        },
+        {
+            icon: `<i class="ri-quill-pen-fill"></i>`,
+            interest: "writing",
+        },
+        {
+            icon: `<i class="ri-paint-fill"></i>`,
+            interest: "painting",
+        }
+        ],
+        bio: "In the end, we only regret the chances we didn't take.",
+        isFriends: null
+    },
+    {
+        profilePic: "https://images.unsplash.com/photo-1503104834685-7205e8607eb9?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        displayPic: "https://images.unsplash.com/photo-1491349174775-aaafddd81942?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        PendingMessage: 5,
+        location: "Lucknow, India",
+        name: "Priya",
+        age: 21,
+        interest: [{
+            icon: `<i class="ri-music-2-fill"></i>`,
+            interest: "music",
+        },
+        {
+            icon: `<i class="ri-quill-pen-fill"></i>`,
+            interest: "writing",
+        },
+        {
+            icon: `<i class="ri-paint-fill"></i>`,
+            interest: "painting",
+        }
+        ],
+        bio: "The only way to do great work is to love what you do.",
+        isFriends: null
+    },
+    {
+        profilePic: "https://images.unsplash.com/photo-1480406537807-00c0914e8d40?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        displayPic: "https://images.unsplash.com/photo-1506634572416-48cdfe530110?q=80&w=1585&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        PendingMessage: 5,
+        location: "USA, California",
+        name: "Alex Perri",
+        age: 24,
+        interest: [{
+            icon: `<i class="ri-music-2-fill"></i>`,
+            interest: "music",
+        },
+        {
+            icon: `<i class="ri-quill-pen-fill"></i>`,
+            interest: "writing",
+        },
+        {
+            icon: `<i class="ri-paint-fill"></i>`,
+            interest: "painting",
+        }
+        ],
+        bio: "Believe you can and you're halfway there.",
+        isFriends: null
+    },
+     
+];
+
+
+function select(elem) {
+    return document.querySelector(elem);
+}
+
+let curr = 0;
+let isAnimating = false;
+function setData(index){
+    select(".profile-img img").src = user[index]?.profilePic;
+    select(".badge h5").textContent = user[index].pendingMessage;
+    select(".location h3").textContent = user[index].location;
+    select(".age").textContent = user[index].age;
+    select(".name ").textContent = user[index].name;
+
+    let clutter = "";
+
+    user[index].interest.forEach(function (interest) {
+        clutter += `<div class="tag flex gap-3 items-center bg-white/20 px-3 py-1 rounded-full">
+        ${interest.icon}
+        <h3 class="text-sm tracking-tight capitalize">${interest.interest}</h3>
+    </div>`
+    })
+    select(".tags").innerHTML = clutter;
+
+    select(".bio p").textContent = user[curr].bio
+
+}
+
+(function setInitial() {
+    select(".main-card img").src = user[curr].displayPic;
+    select(".incoming-card img").src = user[curr + 1].displayPic;
+
+   
+ setData(curr);
+    curr = 0;
+})()
+
+
+function imageChange() {
+    if (!isAnimating) {
+        isAnimating = true;
+        let tl = gsap.timeline({
+            onComplete: function () {
+                isAnimating = false
+                let mainCard = select(".main-card");
+                let incoming = select(".incoming-card");
+
+                incoming.classList.remove("z-[2]");
+                incoming.classList.remove("z-[3]");
+                incoming.classList.remove("incoming-card");
+
+                mainCard.classList.remove("z-[3]");
+                mainCard.classList.add("z-[2]");
+                gsap.set(mainCard, {
+                    scale: 1,
+                    opacity: 1
+                })
+                if (curr === user.length) curr = 0;
+                select(".main-card img").src = user[curr].displayPic;
+                curr++;
+                mainCard.classList.remove("main-card")
+                incoming.classList.add("main-card");
+                mainCard.classList.add("incoming-card")
+
+            }
+        });
+
+        tl.to(".main-card", {
+            scale: 1.1,
+            opacity: 0, 
+            ease: Power3,
+            duration: .9
+        }, "a")
+        tl.from(".incoming-card", {
+            scale: .9,
+            opacity: 0, 
+            ease: Power3,
+            duration: 1.1
+        }, "a")
+    }
+};
+
+let deny = document.querySelector(".deny");
+let accept = document.querySelector(".accept");
+
+deny.addEventListener("click", function () {
+    imageChange();
+    setData(curr)
+    gsap.from(".details .elements",{
+        y: "100%", 
+        stagger: .06,
+        ease :Power4.easeInOut,
+        duration: 1.5
+    })
+});
+accept.addEventListener("click", function () {
+    imageChange();
+    setData(curr-1)
+    gsap.from(".details .elements",{
+        y: "100%", 
+        stagger: .06,
+        ease :Power4.easeInOut,
+        duration: 1.5
+    })
+});
+
+
+(function containerCreator(){
+    document.querySelectorAll(".elements")
+    .forEach(function(elements){
+       let div =  document.createElement("div");
+       div.classList.add(`${elements.classList[1]}container`, 'overflow-hidden')
+       div.appendChild(elements);
+       select(".details").appendChild(div)
+    })
+})()
+
+
